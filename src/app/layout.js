af8d55b58e763e8f64e5fa2aa0ae351b0e6fc5ce
pascal BONNET <pascalbonnet@MacBook-Pro-de-pascal.local>
2023-09-25 14:21:56 +0200

    Init

---
+++ src/app/layout.js
@@ -0,0 +1,61 @@
+import "./globals.css";
+
+import { Inter, Libre_Baskerville } from "next/font/google";
+import { PrismicPreview } from "@prismicio/next";
+import clsx from "clsx";
+
+import { repositoryName } from "@/prismicio";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const libreBaskerville = Libre_Baskerville({
+  weight: ["400", "700"],
+  style: ["ital"],
+  display: "swap",
+});
+
+/**
+ * @param {{ children: React.ReactNode }}
+ */
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={clsx(inter.className, libreBaskerville.className)}
+    >
+      <body className="overflow-x-hidden antialiased">
+        {/* TODO: Remove the following element once you have read the documentation. */}
+        {process.env.NODE_ENV === "development" && (
+          <div
+            style={{
+              background: "#5163ba",
+              padding: "1rem",
+              textAlign: "center",
+              fontSize: "0.85rem",
+              color: "#fff",
+            }}
+          >
+            <p>
+              <strong>👋 Welcome to your new website!</strong> To customize the
+              code and content of this site,{" "}
+              <a
+                href="https://github.com/prismicio-community/nextjs-starter-prismic-multi-language/tree/master/docs"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textDecoration: "underline" }}
+              >
+                see the documentation
+              </a>
+              . Remove this bar in <code>app/layout.js</code>.
+            </p>
+          </div>
+        )}
+        {children}
+        <PrismicPreview repositoryName={repositoryName} />
+      </body>
+    </html>
+  );
+}
